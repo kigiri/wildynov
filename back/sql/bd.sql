6d31f4f4b659595f0c68c4931427b505d38077bb
benoit <rameaubenoit@hotmail.fr>
2018-06-05 17:36:13 +0200

    database

--- back/sql/bd.sql
+++ back/sql/bd.sql
@@ -1,56 +1,45 @@
 
 DROP DATABASE IF EXISTS  projectynov;
-
 CREATE DATABASE projectynov;
-
 USE projectynov;
+
 CREATE TABLE profile(
-        id         Int NOT NULL ,
+        id         Int  Auto_increment  NOT NULL ,
         name       Varchar (255) NOT NULL ,
         first_name Varchar (255) NOT NULL ,
-        year       ENUM ('2018', '2019') NOT NULL ,
-        Campus     ENUM ('Bordeaux', 'Nantes', 'Paris', 'Aix-en-Provence', 'Lyon') NOT NULL ,
+        year       Date NOT NULL ,
+        campus     Varchar (255) NOT NULL ,
         password   Varchar (255) NOT NULL ,
         email      Varchar (255) NOT NULL ,
-        skill      Varchar (255) NOT NULL ,
+        skill      Integer NOT NULL ,
         created_at Datetime NOT NULL ,
         updated_at Datetime NOT NULL
 	,CONSTRAINT profile_PK PRIMARY KEY (id)
 )ENGINE=InnoDB;
 
 
-#------------------------------------------------------------
-# Table: project
-#------------------------------------------------------------
-
-CREATE TABLE project(
+CREATE TABLE projet(
+        id_profile           Int NOT NULL ,
         id                   Int NOT NULL ,
         title                Varchar (255) NOT NULL ,
         deadline_project     Datetime NOT NULL ,
         deadline_application Datetime NOT NULL ,
-        id_profile              FOREIGN KEY ,
         description          Varchar (255) NOT NULL ,
         state                Bool NOT NULL ,
         created_at           Datetime NOT NULL ,
-        uptated_at           Datetime NOT NULL,
-	,CONSTRAINT project_PK PRIMARY KEY (id),
-    ,CONSTRAINT  FOREIGN KEY (id_profile)  REFERENCES project(id)
+        updated              Datetime NOT NULL ,
+        name                 Varchar (255) NOT NULL ,
+        first_name           Varchar (255) NOT NULL ,
+        year                 Date NOT NULL ,
+        campus               Varchar (255) NOT NULL ,
+        password             Varchar (255) NOT NULL ,
+        email                Varchar (255) NOT NULL ,
+        skill                Integer NOT NULL ,
+        created_at_profile   Datetime NOT NULL ,
+        updated_at           Datetime NOT NULL
+	,CONSTRAINT projet_PK PRIMARY KEY (id_profile,id)
+	,CONSTRAINT projet_profile_FK FOREIGN KEY (id_profile) REFERENCES profile(id)
 )ENGINE=InnoDB;
 
 
-#------------------------------------------------------------
-# Table: Profile_has_project
-#------------------------------------------------------------
-
-CREATE TABLE Profile_has_project(
-        id         Int NOT NULL ,
-        id_profile Int NOT NULL ,
-        updated_at Datetime NOT NULL ,
-        created_at Datetime NOT NULL
-	,CONSTRAINT Profile_has_project_PK PRIMARY KEY (id,id_profile)
-
-	,CONSTRAINT Profile_has_project_project_FK FOREIGN KEY (id) REFERENCES project(id)
-	,CONSTRAINT Profile_has_project_profile0_FK FOREIGN KEY (id_profile) REFERENCES profile(id)
-	
-)ENGINE=InnoDB;
 
