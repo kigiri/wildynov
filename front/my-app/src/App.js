0b52f4ff4adf865c45c1882833cf2e0307fcc765
Souhail <mr.souid@live.fr>
2018-06-13 12:36:09 +0200

    login_front

--- front/my-app/src/App.js
+++ front/my-app/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Login from './components/login/Login';
 import './App.css';
-
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Logo from './components/Logo'
+import Register from './components/login/Register'
 class App extends Component {
   render() {
     return (
       <div>
-        
-          <h1>Projet Ynov</h1>
-      
-      
+{/*         
+          <h1>Projet Ynov</h1> */}
+
+          <MuiThemeProvider>
+          <Logo />
+         < Login />    
+         </MuiThemeProvider> 
       </div>
     );
   }
