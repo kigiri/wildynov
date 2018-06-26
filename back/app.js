71b6d5da42e0a7402f17124e9c5db46bd7c6617b
Clement Denis <le.mikmac@gmail.com>
2018-06-26 16:35:22 +0200

    use route in app.js and remove expressJWT

--- back/app.js
+++ back/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mysql = require('mysql')
 const app = express()
+const userRouter = require('./routes/user/user.js')
 const profilRouter = require('./routes/authcontrol/auth.js')
 const projetsRouter = require('./routes/allprojets/projets.js')
 const addProjetsRouter = require('./routes/allprojets/addProjets.js')
@@ -10,7 +11,6 @@ const cors = require('cors')
 const morgan = require('morgan')
 const nodemailer = require('nodemailer')
 const validator = require('express-validator')
-const expressJWT = require('express-jwt')
 const adminRouter = require('./routes/authcontrol/authAdmin.js')
 
 /////////// Middleware/////////////////////
@@ -20,12 +20,12 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 app.use(validator())
-app.use(expressJWT({ secret: process.env.SECRET_TOKEN }).unless({ path: [ '/auth/signup' ] })) //protect routes
 
 
 ////////////ROUTING////////////////////////
 
 app.use('/auth', profilRouter)
+app.use('/user', userRouter)
 app.use('/allprojets', projetsRouter)
 app.use('/allprojets', addProjetsRouter)
 app.use('/authadmin', adminRouter)
